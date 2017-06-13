afa35ced5a0920ea291e3788a23a551dd72b8e1d
Sam Young <samwilsonyoung@gmail.com>
2017-06-13 22:41:41 +1000

    added pseudocode to more functions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,46 @@
-var buildNewGame = function () {
+var buildNewGame = function (reset) {
   var game = {
     //functions
-    checkIfWon: function() {
-
+    checkIfWon: function(playerNumber) {
+      //check if any patterns touching the most recent move are winners
+      //if yes
+        //set game.winner to <playerNumber>
+        //increment <playerNumber>'s score
+        //present winner's message (includes 'play again?')
+        //
+      //else 
+        //check if only one space remains
+        //if yes
+          //check if this space will give win to <otherPlayer>
+          //if yes
+            //set game.winner to <playerNumber>
+            //increment <playerNumber>'s score
+            //present winner's message (includes 'play again?')
+          //else
+            //set game.winner to "draw"
+            //present draw message (includes 'play again?')
+        //else
+          //switch game.playerCurrentTurn to <otherPlayer>
+          //present message to <otherPlayer> indicating its their turn
     },
     clickSquare: function() {
-
-    },
-    submitPlayerName: function(number) {
-
+      //confirm the clicked square is available to be selected
+      //if so
+        //update square to indicate the square has now been clicked
+        //call checkIfWon(game.playerCurrentTurn)
+      //else
+        //present message to the user to indicate the square is not available
     },
     resetGame: function() {
-      
+      //present confirmation
+      //if confirmed
+        //call buildNewGame(true)
+      //else
+        //continue playing
+    },
+    determineFirstPlayer: function() {
+      //randomly determine who goes first
+      //set game.playerCurrentTurn to this player
     },
     //variables
     tilesSelected: 0,
@@ -38,20 +67,25 @@ var buildNewSession = function () {
      },
     submitPlayerName: function(input,playerNumber) {
       //validate input
-      //if playerOne, set name and call getPlayerName("Two")
-      //if playerTwo, set name and call buildNewGame()
+      //if input accepted
+        //if playerOne
+          //set name and call getPlayerName("Two")
+        //if playerTwo
+          //set name and call buildNewGame()
+      //else if bad input
+        //clear and ask for valid input
     }
     returnHome: function() {
       //present confirmation message to the user
-      //if confirm, return home, otherwise continue playing
+      //if confirmed
+        //return home
+      //else
+        //continue playing
     }, 
     playButton: document.getElementById('playButton'),
     homeButton: document.getElementById('homeButton')
   }
 }
-
-}
-
 
 //event listeners
 session.playButton.addEventListener('click',function() { getPlayerName(nameEntryPlayer) }
